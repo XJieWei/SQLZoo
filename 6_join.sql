6e76fdd63d85c1089f6e81ddc4936490c5e145bc
XJieWei <43016427+XJieWei@users.noreply.github.com>
2019-05-08 13:15:20 -0400

    Update 6_join.sql

--- 6_join.sql
+++ 6_join.sql
@@ -50,12 +50,12 @@ ON goal.matchid = game.id
 WHERE game.stadium = 'National Stadium, Warsaw';
 
 -- # 8. Show names of all players who scored a goal against Germany.
-SELECT DISTINCT goal.player
+SELECT DISTINCT player
 FROM goal
 JOIN game
-ON goal.matchid = game.id
+ON matchid = id
 WHERE goal.teamid != 'GER'
-AND (game.team1 = 'GER' OR game.team2 = 'GER';
+AND (team1 = 'GER' OR team2 = 'GER';
 
 -- # 9. Show teamname and the total number of goals scored.
 SELECT eteam.teamname, COUNT(*)
@@ -74,37 +74,24 @@ GROUP BY game.stadium;
 
 -- # 11. For every match involving 'POL', show the matchid, date and
 -- # the number of goals scored.
-SELECT game.id, game.mdate, COUNT(*)
-FROM game
-JOIN goal
-ON game.id = goal.matchid
-WHERE (game.team1 = 'POL' OR game.team2 = 'POL')
-GROUP BY game.id, game.mdate
-ORDER BY game.id;
+SELECT id, mdate, count(*)
+FROM game JOIN goal ON matchid = id
+WHERE team1 = 'POL' OR team2 = 'POL'
+GROUP BY id, mdate;
 
 -- # 12. For every match where 'GER' scored, show matchid, match date
 -- # and the number of goals scored by 'GER'
-SELECT game.id, game.mdate, COUNT(*)
-FROM game
-JOIN goal
-ON goal.matchid = game.id
-WHERE goal.teamid = 'GER'
-GROUP BY game.id;
+SELECT matchid, mdate, COUNT(*)
+FROM game JOIN goal ON id=matchid
+WHERE teamid = 'GER'
+GROUP BY matchid, mdate;
 
 -- # 13. List every match with the goals scored by each team as shown.
-SELECT game.mdate, 
-       game.team1, 
-       SUM(CASE WHEN goal.teamid = game.team1
-           THEN 1
-           ELSE 0
-           END) AS score1,
-       game.team2,
-       SUM(CASE WHEN goal.teamid = game.team2
-           THEN 1
-           ELSE 0
-           END) AS score2
-FROM game
-JOIN goal
-ON (game.id = goal.matchid)
-GROUP BY game.id
-ORDER BY game.mdate, goal.matchid
+SELECT mdate,
+     team1,
+     SUM(CASE WHEN teamid=team1 THEN 1 ELSE 0 END) AS score1,
+     team2,
+     SUM(CASE WHEN teamid=team2 THEN 1 ELSE 0 END) AS score2
+FROM game LEFT JOIN goal ON matchid = id
+GROUP BY mdate,team1,team2
+ORDER BY mdate,matchid, team1, team2;
